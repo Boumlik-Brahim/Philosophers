57ad6dab5e70bfb9dd54726461afe8aef50e765e
Boumlik-Brahim <boumlik01brahim@gmail.com>
2022-04-27 06:34:26 +0000

    errors chng

--- philo_mandatory/philosophers.c
+++ philo_mandatory/philosophers.c
@@ -33,9 +33,9 @@ void *routine(void *arg)
         print_state(philo, philo[i].id + 1,"has taken a fork\n");
         print_state(philo, philo[i].id + 1,"has taken a fork\n");
         print_state(philo, philo[i].id + 1,"is eating\n");
-        usleep(philo->shared_data->time_to_eat);
-        pthread_mutex_unlock(&philo[philo->right_fork].fork_Mutex);
+        usleep(philo->shared_data->time_to_eat * 1000);
         pthread_mutex_unlock(&philo[philo->left_fork].fork_Mutex);
+        pthread_mutex_unlock(&philo[philo->right_fork].fork_Mutex);
     }
     return NULL;
 }
@@ -47,22 +47,12 @@ int main(int ac, char **av)
     int     i;
 
     if (ac == 2 && !ft_strncmp(av[1], "--help", 6))
-    {
-        ft_putstr_fd("\"philosophers\" requires 4 argument and 1 [optional] passed as fllows:\n ./philo arg1 arg2 arg3 arg4 [arg 5]\n", 1);
-        return (0);
-    }
+        return (ft_putstr_fd("\"philosophers\" requires 4 argument and 1 [optional] passed as fllows:\n ./philo arg1 arg2 arg3 arg4 [arg 5]\n", 1), 0);
     else if (ac != 5 && ac != 6)
-    {
-        ft_handle_error("INVALID ARGS TRY \"--help\" FOR MORE INFORMATIONS.\n");
-        return (0);
-    }
+        return (ft_handle_error("INVALID ARGS TRY \"--help\" FOR MORE INFORMATIONS.\n"), -1);
     args = ft_join_args(av);
     if (ft_init_data(&data, args, ac, av) == -1)
-    {
-        ft_handle_error("INVALID ARGS TRY \"--help\" FOR MORE INFORMATIONS.\n");
-        return (0);
-    }
-
+        return (ft_handle_error("INVALID ARGS TRY \"--help\" FOR MORE INFORMATIONS.\n"), -1);
 
     ft_init_philo(data.philo,&data);
     i = 0;
@@ -76,7 +66,7 @@ int main(int ac, char **av)
     while (i < data.nbr_philosophers)
     {
         if (pthread_create(&data.philo[i].thread, NULL, &routine, (void*)&data.philo[i]) != 0)
-            ft_handle_error("THREAD ERROR");
+            return (ft_handle_error("THREAD ERROR"), -1);
         pthread_detach(data.philo[i].thread);
         i++;
     }
@@ -86,7 +76,7 @@ int main(int ac, char **av)
     //     pthread_mutex_destroy(&data.philo[i].fork_Mutex);
     //     i++;
     // }
-    //free_data(args);
+    free_data(args);
     while (1){}
     return (0);    
 }
